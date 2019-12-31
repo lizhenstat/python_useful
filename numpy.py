5bae79ecbeff5eb391bc164f5ce8f0967835195b
lizhen <41462408+lizhenstat@users.noreply.github.com>
2019-12-31 15:16:58 +0800

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -27,3 +27,27 @@ def myfunc(a,b, *args, **kwargs):
 myfunc(1,2,3,4,e='hi',f='we')
 
 
+# python lambda functions
+# x = 'False'
+f = lambda x: (str(x).lower() == 'true')
+f('True')
+
+
+# python subprocess with arguments
+# -->pickle_dump & pickle_load 
+# 如果需要用命令行传object对象时，可以选择传object的保存路径
+import pickle
+a = {'hello': 'world'}
+with open('filename.pickle', 'wb') as handle:
+    pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
+with open('filename.pickle', 'rb') as handle:
+    b = pickle.load(handle)
+print (a == b)
+
+
+# 子程序要import上一级文件夹的文件
+import sys
+sys.path.append("E:/CondenseNet/condensenet_new_2")
+import models
+# 不能用 from ..  import models
+# 因为python不认得当前的工作路径
