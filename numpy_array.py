0fbaee95ac0f900e3505701b46b3a3a8b301dc3e
lizhen <41462408+lizhenstat@users.noreply.github.com>
2020-06-18 09:55:16 +0800

    Update numpy_array.py

--- numpy_array.py
+++ numpy_array.py
@@ -11,3 +11,17 @@ a = np.array([1,2,3])
 b = np.array([[1],[2],[3]])
 a+b # 结果是3x3的矩阵 逐行逐列分别相加
 
+# (3,3,5) * (5,1) 等价于 (9,5) * (5,1)
+import numpy as np
+import torch
+a = np.array([[1,1,1],[2,2,2],[3,3,3]])
+a = torch.tensor(a)
+a = a.unsqueeze(2)
+a = a.repeat(1,1,5)
+print(a)
+w = torch.tensor(np.array([1,2,3,4,5])).unsqueeze(1)
+print(torch.matmul(a,w))
+
+# 等价于以下
+b = a.reshape(3*3,5)
+torch.matmul(b,w).reshape(3,3,1)
